81f3f9f0dbae92bf43221e387b36e63031ecc3d4
riskiardian <99702053+kiidspyro@users.noreply.github.com>
2022-09-22 08:08:39 +0700

    Add files via upload

---
+++ PRAKTIKUM 1/NOMOR 11.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <?php
+        $val1 = "Jika sedang belajar, ";
+        $val2 = "Harap jangan ribut! ";
+        echo "Menggabungkan 2 string val1 dengan val2 :";
+
+        echo $val1.$val2;
+        echo "<br>";
+        ?>
+    </body>
+</html>
---
+++ PRAKTIKUM 1/NOMOR 2.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title> Contoh Skrip PHP </title>
+    </head>
+    <body>
+        <?php
+            $nama = "Riski Ardian Putra";
+            echo "$nama berkata \"Katakan namamu!\"";
+            ?>
+    </body>
+</html>
